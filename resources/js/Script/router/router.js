fc3618654fbcced988b56fb9c6d9f453dfe4390d
Enzo Innocenzi <enzo.inno@gmail.com>
2019-10-12 00:11:52 +0200

    Added back default router.js

---
+++ resources/js/Script/router/router.js
@@ -0,0 +1,16 @@
+var Ziggy = {
+  namedRoutes: {},
+  baseUrl: 'http://localhost/',
+  baseProtocol: 'http',
+  baseDomain: 'localhost',
+  basePort: false,
+  defaultParameters: [],
+};
+
+if (typeof window.Ziggy !== 'undefined') {
+  for (var name in window.Ziggy.namedRoutes) {
+    Ziggy.namedRoutes[name] = window.Ziggy.namedRoutes[name];
+  }
+}
+
+export { Ziggy };
