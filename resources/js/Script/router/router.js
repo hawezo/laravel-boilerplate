a250fe414237384ee19d8e792d3a34445ce30211
Enzo Innocenzi <enzo.inno@gmail.com>
2019-10-12 00:14:16 +0200

    Fix: added back this boilerplate's routes

--- resources/js/Script/router/router.js
+++ resources/js/Script/router/router.js
@@ -1,16 +1,18 @@
-var Ziggy = {
-  namedRoutes: {},
-  baseUrl: 'http://localhost/',
-  baseProtocol: 'http',
-  baseDomain: 'localhost',
-  basePort: false,
-  defaultParameters: [],
-};
+    var Ziggy = {
+        namedRoutes: {"index":{"uri":"\/","methods":["GET","HEAD"],"domain":null}},
+        baseUrl: 'http://laravel-test.dev/',
+        baseProtocol: 'http',
+        baseDomain: 'laravel-test.dev',
+        basePort: false,
+        defaultParameters: []
+    };
 
-if (typeof window.Ziggy !== 'undefined') {
-  for (var name in window.Ziggy.namedRoutes) {
-    Ziggy.namedRoutes[name] = window.Ziggy.namedRoutes[name];
-  }
-}
+    if (typeof window.Ziggy !== 'undefined') {
+        for (var name in window.Ziggy.namedRoutes) {
+            Ziggy.namedRoutes[name] = window.Ziggy.namedRoutes[name];
+        }
+    }
 
-export { Ziggy };
+    export {
+        Ziggy
+    }
