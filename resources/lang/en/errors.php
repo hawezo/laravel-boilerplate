92f8ee8624686e3b7660c923e287d2fb87ab226b
Enzo Innocenzi <enzo.inno@gmail.com>
2019-10-12 21:53:21 +0200

    Changed error message

--- resources/lang/en/errors.php
+++ resources/lang/en/errors.php
@@ -8,7 +8,7 @@ return [
         500 => 'Internal Server Error',
     ],
     'description' => [
-        404 => 'The resource you attempted to access is not available.',
+        404 => 'The resource you attempted to access could not be found.',
         503 => 'The service is currently unavailable. Please try again later.',
         403 => 'You are not authorized to access this resource.',
         500 => 'An unexpected occured. Sorry for the inconvenience.',
